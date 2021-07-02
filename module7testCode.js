2bdcae0d3c865d0f7dc3d9e9a7f302b9718ab0c5
DustinGentry <dustiumus@gmail.com>
2021-07-02 12:15:39 -0600

    initial commit

---
+++ module7testCode.js
@@ -0,0 +1,13 @@
+function charSpecial() { 
+    const borderlands = 'Tiny Tina is the best there Is!'
+    if (borderlands.includes('Tiny Tina')){
+        return 'The biggest Boom Around!'}
+    else {
+        return 'Tina dont like you'
+    };
+ 
+};
+
+
+
+
